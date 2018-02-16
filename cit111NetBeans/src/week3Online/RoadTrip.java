61d74f4fcfefa230dc604f63ebdc1efe7bdeef9a
Eric Darsow <edarsow@andrew.cmu.edu>
2018-02-15 23:06:10 -0500

    finished raod trip class

--- cit111NetBeans/src/week3Online/RoadTrip.java
+++ cit111NetBeans/src/week3Online/RoadTrip.java
@@ -81,8 +81,68 @@ public class RoadTrip {
         System.out.println("I have $" + cashOnHand + " to spend on this trip");
         System.out.println(currentNumberOfPassengers + " passengers in car");
         System.out.println("Destination Reached? " + destinationReached);
-     
-       
-                
+        
+        // Solution to exercise for completing road trip
+        
+        //Step 1 in guide: set leg distance to 500 miles
+        legDistance = 500.00;
+        
+        // Step 2: Sumulate finding a pair of hitchhikers and check car status
+        System.out.println("Oh, Two more WestBound travelers! Check car sta");
+        if(carFull == false){
+            System.out.println("Car is not full--picking up hitchers");
+            currentNumberOfPassengers = currentNumberOfPassengers + 2;
+        }
+        
+        // Step 3: Calculate fuel price for driving 500.0 more miles
+        gasPriceForLeg = (legDistance / NEDS_MILES_PER_GALLON) * AVERAGE_GAS_PRICE;
+        cashOnHand = cashOnHand - gasPriceForLeg;
+        System.out.println("Amount spent on gas leg 2: " + gasPriceForLeg);
+        
+        // Step 4: Print out leg two data:
+        System.out.println();
+        System.out.println("****Variable stats at end of the leg 2****");
+        System.out.println("Distance To Destination: " + distanceToMoabUtah);
+        System.out.println("Full Car? " + carFull + "; Current odometer: " + tripOdometer);
+        System.out.println("I have $" + cashOnHand + " to spend on this trip");
+        System.out.println(currentNumberOfPassengers + " passengers in car");
+        System.out.println("Destination Reached? " + destinationReached);
+        
+        // Step 5: Get remaining distance to utah as leg distance
+        legDistance = distanceToMoabUtah - legDistance;
+        
+        // Step 6: More hitchikers
+        System.out.println("More hitchers: another pair! Can we fit them?");
+        
+        int numHitchers = 2;
+        // note this was a different method for testing capacity that's more
+        // robust than the above method that doesn't take into account
+        // the number of hitchers we might pick up
+        if(currentNumberOfPassengers + numHitchers <= maxPassengers){
+            System.out.println("Able to pick up hitchers");
+            currentNumberOfPassengers = currentNumberOfPassengers + numHitchers;
+        } else  {
+            System.out.println("Too many folks--can't take ya, sorry!");
+        }
+        
+        // Step 7: pay for gas for rest of trip
+        gasPriceForLeg = (legDistance / NEDS_MILES_PER_GALLON) * AVERAGE_GAS_PRICE;
+        cashOnHand = cashOnHand - gasPriceForLeg;
+        System.out.println("Cash paid for gas on final leg: " + gasPriceForLeg);
+        
+        // Step 8: Arrive! and toggle destination reached var
+        destinationReached = true;
+        
+        // Step 9: Print out final stats:
+        
+        System.out.println();
+        System.out.println("****Variable stats at end of the leg 3****");
+        System.out.println("Distance To Destination: " + distanceToMoabUtah);
+        System.out.println("Full Car? " + carFull + "; Current odometer: " + tripOdometer);
+        System.out.println("I have $" + cashOnHand + " to spend on this trip");
+        System.out.println(currentNumberOfPassengers + " passengers in car");
+        System.out.println("Destination Reached? " + destinationReached);
+        
+        
     } // close class main
 } // close class RoadTrip
